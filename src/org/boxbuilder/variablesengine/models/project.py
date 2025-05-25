a37bfa3f26be83173bdab1a5abd7e462f6113773
shr22avan <shravan1993@gmail.com>
2025-05-25 12:47:21 +0530

    Adding entity model

--- src/org/boxbuilder/variablesengine/models/project.py
+++ src/org/boxbuilder/variablesengine/models/project.py
@@ -2,9 +2,12 @@ from typing import List
 
 from pydantic import BaseModel, Field
 
+from org.boxbuilder.variablesengine.models.entity import Entity
 from org.boxbuilder.variablesengine.models.variable import Variable
 
 
 class Project(BaseModel):
+    id: str
     name: str
+    entities: List[Entity] = Field(default_factory=list)
     variables: List[Variable] = Field(default_factory=list)
--- src/org/boxbuilder/variablesengine/models/variable.py
+++ src/org/boxbuilder/variablesengine/models/variable.py
@@ -1,10 +1,13 @@
-from typing import Optional
+from typing import Optional, List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Variable(BaseModel):
+    id: str
     name: str
     is_input: bool
     is_persisted: Optional[bool] = None
     function_name: Optional[str] = None
+    entity_id: str
+    input_variables: List[str] = Field(default_factory=list)
